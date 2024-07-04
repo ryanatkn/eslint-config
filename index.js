72c5efc819587d4f4bcaec87e81ee584004472af
Ryan Atkinson <mail@ryanatkn.com>
2024-07-03 21:30:10 -0600

    wip

--- index.js
+++ index.js
@@ -271,7 +271,34 @@ const configs = [
 	},
 ];
 
-export default ts.config(...configs);
+const warn_instead_of_error = (configs) => {
+	return configs.map((config) => {
+		if (!config.rules) return config;
+		const c = {...config, rules: {...config.rules}};
+		for (const [rule, value] of Object.entries(c.rules)) {
+			if (value === 'error' || value === 2) {
+				c.rules[rule] = 'warn';
+			}
+			if (Array.isArray(value) && (value[0] === 'error' || value[0] === 2)) {
+				c.rules[rule][0] = 'warn';
+			}
+		}
+		return c;
+	});
+};
+
+const final_configs = warn_instead_of_error(configs);
+
+export default ts.config(...final_configs);
+
+const is_problem = (v) => {
+	if (v === 'off') return false;
+	if (v === 'error') return true;
+	if (v === 'warn') return true;
+	if (typeof v === 'number') return v !== 0;
+	if (Array.isArray(v)) return is_problem(v[0]);
+	throw new Error(`Unknown value ${v}`);
+};
 
 /**
  * @param {import('eslint').Linter.FlatConfig[]} configs
@@ -283,7 +310,6 @@ const lint_configs = (configs) => {
 			return map;
 		})
 		.filter((m) => !!m);
-	console.log(`rule_maps`, rule_maps);
 
 	const all_rules = new Map();
 	const conflicts = [];
@@ -296,320 +322,19 @@ const lint_configs = (configs) => {
 			}
 			// There's already a rule with this name, do they conflict?
 			// If not it's fine, if so it's unnecessary.
-			console.log(`rule`, rule, `value`, value);
-			if (existing.value === value) {
-				conflicts.push({rule, existing, name, value});
+			if (is_problem(existing.value) === is_problem(value)) {
+				if (name.startsWith('local_')) {
+					conflicts.push({rule, existing, name, value});
+				}
+				continue;
 			}
+			all_rules.set(rule, {name, rule, value}); // override the existing
 		}
 	}
-	console.log(`conflicts`, conflicts); // TODO BLOCK isn't correct yet
+	console.log(
+		`conflicts`,
+		conflicts.map((c) => c.rule),
+	); // TODO BLOCK isn't correct yet
 	process.exit();
 };
-lint_configs(configs);
-
-/*
-ts_config [
-  {
-    rules: {
-      'constructor-super': 'error',
-      'for-direction': 'error',
-      'getter-return': 'error',
-      'no-async-promise-executor': 'error',
-      'no-case-declarations': 'error',
-      'no-class-assign': 'error',
-      'no-compare-neg-zero': 'error',
-      'no-cond-assign': 'error',
-      'no-const-assign': 'error',
-      'no-constant-binary-expression': 'error',
-      'no-constant-condition': 'error',
-      'no-control-regex': 'error',
-      'no-debugger': 'error',
-      'no-delete-var': 'error',
-      'no-dupe-args': 'error',
-      'no-dupe-class-members': 'error',
-      'no-dupe-else-if': 'error',
-      'no-dupe-keys': 'error',
-      'no-duplicate-case': 'error',
-      'no-empty': 'error',
-      'no-empty-character-class': 'error',
-      'no-empty-pattern': 'error',
-      'no-empty-static-block': 'error',
-      'no-ex-assign': 'error',
-      'no-extra-boolean-cast': 'error',
-      'no-fallthrough': 'error',
-      'no-func-assign': 'error',
-      'no-global-assign': 'error',
-      'no-import-assign': 'error',
-      'no-invalid-regexp': 'error',
-      'no-irregular-whitespace': 'error',
-      'no-loss-of-precision': 'error',
-      'no-misleading-character-class': 'error',
-      'no-new-native-nonconstructor': 'error',
-      'no-nonoctal-decimal-escape': 'error',
-      'no-obj-calls': 'error',
-      'no-octal': 'error',
-      'no-prototype-builtins': 'error',
-      'no-redeclare': 'error',
-      'no-regex-spaces': 'error',
-      'no-self-assign': 'error',
-      'no-setter-return': 'error',
-      'no-shadow-restricted-names': 'error',
-      'no-sparse-arrays': 'error',
-      'no-this-before-super': 'error',
-      'no-undef': 'error',
-      'no-unexpected-multiline': 'error',
-      'no-unreachable': 'error',
-      'no-unsafe-finally': 'error',
-      'no-unsafe-negation': 'error',
-      'no-unsafe-optional-chaining': 'error',
-      'no-unused-labels': 'error',
-      'no-unused-private-class-members': 'error',
-      'no-unused-vars': 'error',
-      'no-useless-backreference': 'error',
-      'no-useless-catch': 'error',
-      'no-useless-escape': 'error',
-      'no-with': 'error',
-      'require-yield': 'error',
-      'use-isnan': 'error',
-      'valid-typeof': 'error'
-    },
-		*/
-// files: [ '**/*.js', '**/*.ts', '**/*.svelte' ]
-/*
-  },
-  {
-    name: 'typescript-eslint/base',
-    languageOptions: { parser: [Object], sourceType: 'module' },
-    plugins: { '@typescript-eslint': [Object] },
-		*/
-// files: [ '**/*.js', '**/*.ts', '**/*.svelte' ]
-/*
-  },
-  {
-    name: 'typescript-eslint/strict-type-checked',
-    rules: {
-      '@typescript-eslint/await-thenable': 'error',
-      '@typescript-eslint/ban-ts-comment': [Array],
-      'no-array-constructor': 'off',
-      '@typescript-eslint/no-array-constructor': 'error',
-      '@typescript-eslint/no-array-delete': 'error',
-      '@typescript-eslint/no-base-to-string': 'error',
-      '@typescript-eslint/no-confusing-void-expression': 'error',
-      '@typescript-eslint/no-duplicate-enum-values': 'error',
-      '@typescript-eslint/no-duplicate-type-constituents': 'error',
-      '@typescript-eslint/no-dynamic-delete': 'error',
-      '@typescript-eslint/no-empty-object-type': 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-extra-non-null-assertion': 'error',
-      '@typescript-eslint/no-extraneous-class': 'error',
-      '@typescript-eslint/no-floating-promises': 'error',
-      '@typescript-eslint/no-for-in-array': 'error',
-      'no-implied-eval': 'off',
-      '@typescript-eslint/no-implied-eval': 'error',
-      '@typescript-eslint/no-invalid-void-type': 'error',
-      '@typescript-eslint/no-meaningless-void-operator': 'error',
-      '@typescript-eslint/no-misused-new': 'error',
-      '@typescript-eslint/no-misused-promises': 'error',
-      '@typescript-eslint/no-mixed-enums': 'error',
-      '@typescript-eslint/no-namespace': 'error',
-      '@typescript-eslint/no-non-null-asserted-nullish-coalescing': 'error',
-      '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
-      '@typescript-eslint/no-non-null-assertion': 'error',
-      '@typescript-eslint/no-redundant-type-constituents': 'error',
-      '@typescript-eslint/no-require-imports': 'error',
-      '@typescript-eslint/no-this-alias': 'error',
-      '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
-      '@typescript-eslint/no-unnecessary-condition': 'error',
-      '@typescript-eslint/no-unnecessary-template-expression': 'error',
-      '@typescript-eslint/no-unnecessary-type-arguments': 'error',
-      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
-      '@typescript-eslint/no-unnecessary-type-constraint': 'error',
-      '@typescript-eslint/no-unsafe-argument': 'error',
-      '@typescript-eslint/no-unsafe-assignment': 'error',
-      '@typescript-eslint/no-unsafe-call': 'error',
-      '@typescript-eslint/no-unsafe-declaration-merging': 'error',
-      '@typescript-eslint/no-unsafe-enum-comparison': 'error',
-      '@typescript-eslint/no-unsafe-function-type': 'error',
-      '@typescript-eslint/no-unsafe-member-access': 'error',
-      '@typescript-eslint/no-unsafe-return': 'error',
-      '@typescript-eslint/no-unsafe-unary-minus': 'error',
-      'no-unused-expressions': 'off',
-      '@typescript-eslint/no-unused-expressions': 'error',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'error',
-      'no-useless-constructor': 'off',
-      '@typescript-eslint/no-useless-constructor': 'error',
-      '@typescript-eslint/no-wrapper-object-types': 'error',
-      'no-throw-literal': 'off',
-      '@typescript-eslint/only-throw-error': 'error',
-      '@typescript-eslint/prefer-as-const': 'error',
-      '@typescript-eslint/prefer-literal-enum-member': 'error',
-      '@typescript-eslint/prefer-namespace-keyword': 'error',
-      'prefer-promise-reject-errors': 'off',
-      '@typescript-eslint/prefer-promise-reject-errors': 'error',
-      '@typescript-eslint/prefer-reduce-type-parameter': 'error',
-      '@typescript-eslint/prefer-return-this-type': 'error',
-      'require-await': 'off',
-      '@typescript-eslint/require-await': 'error',
-      '@typescript-eslint/restrict-plus-operands': [Array],
-      '@typescript-eslint/restrict-template-expressions': [Array],
-      '@typescript-eslint/triple-slash-reference': 'error',
-      '@typescript-eslint/unbound-method': 'error',
-      '@typescript-eslint/unified-signatures': 'error',
-      '@typescript-eslint/use-unknown-in-catch-callback-variable': 'error'
-    },
-		*/
-// files: [ '**/*.js', '**/*.ts', '**/*.svelte' ]
-/*
-  },
-  {
-    name: 'typescript-eslint/base',
-    languageOptions: { parser: [Object], sourceType: 'module' },
-    plugins: { '@typescript-eslint': [Object] },
-		*/
-// files: [ '**/*.js', '**/*.ts', '**/*.svelte' ]
-/*
-  },
-  {
-	*/
-// files: [ '**/*.js', '**/*.ts', '**/*.svelte' ],
-/*
-    rules: {
-      'constructor-super': 'off',
-      'getter-return': 'off',
-      'no-const-assign': 'off',
-      'no-dupe-args': 'off',
-      'no-dupe-class-members': 'off',
-      'no-dupe-keys': 'off',
-      'no-func-assign': 'off',
-      'no-import-assign': 'off',
-      'no-new-symbol': 'off',
-      'no-new-native-nonconstructor': 'off',
-      'no-obj-calls': 'off',
-      'no-redeclare': 'off',
-      'no-setter-return': 'off',
-      'no-this-before-super': 'off',
-      'no-undef': 'off',
-      'no-unreachable': 'off',
-      'no-unsafe-negation': 'off',
-      'no-var': 'error',
-      'prefer-const': 'error',
-      'prefer-rest-params': 'error',
-      'prefer-spread': 'error'
-    },
-    name: 'typescript-eslint/eslint-recommended'
-  },
-  {
-    name: 'typescript-eslint/stylistic-type-checked',
-    rules: {
-      '@typescript-eslint/adjacent-overload-signatures': 'error',
-      '@typescript-eslint/array-type': 'error',
-      '@typescript-eslint/ban-tslint-comment': 'error',
-      '@typescript-eslint/class-literal-property-style': 'error',
-      '@typescript-eslint/consistent-generic-constructors': 'error',
-      '@typescript-eslint/consistent-indexed-object-style': 'error',
-      '@typescript-eslint/consistent-type-assertions': 'error',
-      '@typescript-eslint/consistent-type-definitions': 'error',
-      'dot-notation': 'off',
-      '@typescript-eslint/dot-notation': 'error',
-      '@typescript-eslint/no-confusing-non-null-assertion': 'error',
-      'no-empty-function': 'off',
-      '@typescript-eslint/no-empty-function': 'error',
-      '@typescript-eslint/no-inferrable-types': 'error',
-      '@typescript-eslint/non-nullable-type-assertion-style': 'error',
-      '@typescript-eslint/prefer-find': 'error',
-      '@typescript-eslint/prefer-for-of': 'error',
-      '@typescript-eslint/prefer-function-type': 'error',
-      '@typescript-eslint/prefer-includes': 'error',
-      '@typescript-eslint/prefer-nullish-coalescing': 'error',
-      '@typescript-eslint/prefer-optional-chain': 'error',
-      '@typescript-eslint/prefer-regexp-exec': 'error',
-      '@typescript-eslint/prefer-string-starts-ends-with': 'error'
-    },
-		*/
-// files: [ '**/*.js', '**/*.ts', '**/*.svelte' ]
-/*
-  },
-  {
-	*/
-// files: [ '**/*.js', '**/*.ts', '**/*.svelte' ],
-/*
-    languageOptions: { globals: [Object], parserOptions: [Object] }
-  }
-]
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-svelte_config [
-  {
-    name: 'svelte:base:setup-plugin',
-    plugins: { svelte: [Getter] },
-		*/
-// files: [ '**/*.svelte' ]
-/*
-  },
-  {
-    name: 'svelte:base:setup-for-svelte',
-		*/
-// files: [ '**/*.svelte' ],
-/*
-    languageOptions: { parser: [Object] },
-    rules: {
-      'no-inner-declarations': 'off',
-      'no-self-assign': 'off',
-      'svelte/comment-directive': 'error',
-      'svelte/system': 'error'
-    },
-    processor: 'svelte/svelte'
-  },
-  {
-    name: 'svelte:recommended:rules',
-    rules: {
-      'svelte/comment-directive': 'error',
-      'svelte/no-at-debug-tags': 'warn',
-      'svelte/no-at-html-tags': 'error',
-      'svelte/no-dupe-else-if-blocks': 'error',
-      'svelte/no-dupe-style-properties': 'error',
-      'svelte/no-dynamic-slot-name': 'error',
-      'svelte/no-inner-declarations': 'error',
-      'svelte/no-not-function-handler': 'error',
-      'svelte/no-object-in-text-mustaches': 'error',
-      'svelte/no-shorthand-style-property-overrides': 'error',
-      'svelte/no-unknown-style-directive-property': 'error',
-      'svelte/no-unused-svelte-ignore': 'error',
-      'svelte/system': 'error',
-      'svelte/valid-compile': 'error'
-    },
-		*/
-// files: [ '**/*.svelte' ]
-/*
-  },
-  {
-	*/
-// files: [ '**/*.svelte' ],
-/*
-    languageOptions: { parserOptions: [Object] }
-  }
-]
-*/
+lint_configs(final_configs);
