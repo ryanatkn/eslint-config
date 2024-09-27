bf8a1322e5006f1410c2decd261686d2b93cb484
Ryan Atkinson <mali[D[Cmail@ryanatkn.com>
2024-09-27 16:29:38 -0600

    fix type

---
+++ .changeset/lovely-cars-tell.md
@@ -0,0 +1,5 @@
+---
+'@ryanatkn/eslint-config': patch
+---
+
+fix type
--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import type {Linter} from 'eslint';
 // TODO hacky - probably more than just `rules` is required, and there's no rules type safety here
 type Config_With_Rules = Linter.Config & {rules: Linter.RulesRecord};
 
-export const configs: Config_With_Rule[];
+export const configs: Config_With_Rules[];
 
 export const ts_config: Config_With_Rules;
 
